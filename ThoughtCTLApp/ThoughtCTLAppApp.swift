46aeb3ab6ab78fb2150eea60cfe1f07c954d247e
Dipendra Dubey <dipendradubey@gmail.com>
2023-12-13 11:08:40 +0530

    Initial Commit

---
+++ ThoughtCTLApp/ThoughtCTLAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThoughtCTLAppApp.swift
+//  ThoughtCTLApp
+//
+//  Created by Dipendra Dubey on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ThoughtCTLAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
